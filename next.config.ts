1d246d15cf8d33e061dd9b4e4c4f3b6a08ebd214
MARTYapp <ericdanieladler@gmail.com>
2025-10-20 16:16:34 -0400

    chore: clean up eslint config and remove redundant echo file

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
-import * as path from 'path';
-import type { NextConfig } from 'next';
-import type { Configuration } from 'webpack';
+import path from "path";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config: Configuration) {
-    const alias = config.resolve!.alias as { [key: string]: string };
-    alias['@'] = path.resolve(__dirname);
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
     return config;
   },
 };
