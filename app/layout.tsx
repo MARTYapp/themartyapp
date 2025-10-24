e30d4b179f574330b84c07760c90ac40af6f185f
MARTYapp <ericdanieladler@gmail.com>
2025-10-24 17:14:26 -0400

    redox2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
-import "./globals.css"
+import React from "react";
+import "./globals.css";
 
 export const metadata = {
   title: "The MARTY App",
   description: "MARTY ≠ THERAPY — An emotionally fluent modern ecosystem.",
-}
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="antialiased">{children}</body>
     </html>
-  )
+  );
 }
